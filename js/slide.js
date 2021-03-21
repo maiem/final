679d6ea4de8a35bd997452c95833a0330d36ef82
Maianh0101 <hathimaianh03@gmail.com>
2021-03-21 13:40:20 +0700

    upload

--- js/slide.js
+++ js/slide.js
@@ -69,3 +69,15 @@ $(async function () {
     ))
   
   }
+
+
+  //slide
+  $(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
